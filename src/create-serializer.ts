9b2795a6665d9348999cda6d05786f74cfb4986b
Ryan Elliott-Potter <ryan@ryanep.com>
2020-12-16 20:06:54 +0000

    Upgrade all dependencies

--- src/create-serializer.ts
+++ src/create-serializer.ts
@@ -2,13 +2,11 @@ import { ASTNode } from 'graphql';
 import { isGraphqlSchema } from './utils';
 import { format } from './format';
 
-export const createSerializer = (): jest.SnapshotSerializerPlugin => {
-  return {
-    test(node: ASTNode): boolean {
-      return isGraphqlSchema(node);
-    },
-    print(node: ASTNode): string {
-      return format(node);
-    },
-  };
-};
+export const createSerializer = (): jest.SnapshotSerializerPlugin => ({
+  test(node: ASTNode): boolean {
+    return isGraphqlSchema(node);
+  },
+  print(node: ASTNode): string {
+    return format(node);
+  },
+});
